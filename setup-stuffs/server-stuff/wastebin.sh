1fa90a76095ffe0fe69fe69c888aef7828d76946
garrettjwilke <garrett@system76.com>
2025-01-27 16:55:23 -0700

    added setup-stuffs

---
+++ setup-stuffs/server-stuff/wastebin.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export WASTEBIN_DATABASE_PATH="/home/homeless/wastebin/db"
+export WASTEBIN_BASE_URL="http://homeless.wtf:8088"
+export WASTEBIN_TITLE="homeless.wtf paste"
+export WASTEBIN_ADDRESS_PORT="0.0.0.0:8086"
+./home/homeless/wastebin/wastebin

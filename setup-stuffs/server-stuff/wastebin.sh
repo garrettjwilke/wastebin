0a6b2d2778b033d225380c88cf0df78ace33340b
garrettjwilke <garrett@system76.com>
2025-09-16 21:36:01 -0600

    reverted to defaults

--- setup-stuffs/server-stuff/wastebin.sh
+++ setup-stuffs/server-stuff/wastebin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export WASTEBIN_DATABASE_PATH="/opt/wastebin/db"
-export WASTEBIN_BASE_URL="http://127.0.0.1:8088"
-export WASTEBIN_TITLE="wastebin thingy"
-export WASTEBIN_ADDRESS_PORT="127.0.0.1:8088"
-./opt/wastebin/wastebin
+export WASTEBIN_DATABASE_PATH="/home/homeless/wastebin/db"
+export WASTEBIN_BASE_URL="http://homeless.wtf:8088"
+export WASTEBIN_TITLE="homeless.wtf paste"
+export WASTEBIN_ADDRESS_PORT="0.0.0.0:8086"
+./home/homeless/wastebin/wastebin
